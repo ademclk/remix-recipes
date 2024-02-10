ae9af484814e6aca660f30bb21991713575371a5
Adem Onur Çelik <ademonurcelik@icloud.com>
2024-02-10 21:21:19 +0300

    feat: special components of remix

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -9,9 +9,14 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Remix Recipes" },
+    { name: "description", content: "Welcome to the Remix Recipes app!" },
+  ];
+};
+
 
 export default function App() {
   return (
@@ -23,6 +28,12 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="app">App</Link>
+          <Link to="discover">Discover</Link>
+          <Link to="settings">Settings</Link>
+        </nav>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
